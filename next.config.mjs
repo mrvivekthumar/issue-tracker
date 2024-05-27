31c3f8e1355d32400817ecd299239c1185a70627
mrvivekthumar <mrvivekthumar@gmail.com>
2024-05-28 01:56:13 +0530

    make avatar not loading in next.config.mjs code

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,20 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    // ONLY USE WHEN THERE A IS FEATCHING ISSUE  IN GOOGLE AVATAR
+
+    // async headers() {
+    //     return [
+    //         {
+    //             source: "/:path*",
+    //             headers: [
+    //                 { key: 'referrer-policy', value: 'no-referrer' }
+    //             ]
+    //         }
+    //     ]
+    // }
+
+
+
+};
 
 export default nextConfig;
